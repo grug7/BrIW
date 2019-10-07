625c9c3543e584dd181d394d9e37b0faeddb4084
Greg Ford <greg.ford@infinityworks.com>
2019-10-07 13:37:34 +0100

    fixed orders table so now correct info shows in correct columns

--- static/rounds.js
+++ static/rounds.js
@@ -11,14 +11,32 @@ function getRoundOrdersAPI(round_id) {
 }
 
 function generateTableBody(table, data) {
-    console.log(`data: ${data}`);
-    for (let element of data) {
-        console.log(`element: ${element}`)
+    for (let order of data) {
         let row = table.insertRow();
-        for (let key in element) {
-            console.log(`key: ${key}`);
+        
+        for (let i = 0; i < Object.keys(order).length; i++) {
+            //key is each object inside the order (orderid, person, drink)
             let cell = row.insertCell();
-            let text = document.createTextNode(element[key]);
+            let cellValue;
+
+            switch (i) {
+              case 0:
+                console.log("zero");
+                console.log(order["id"])
+                cellValue = order["id"];
+                break;
+              case 1:
+                console.log("one");
+                person = order["person"];
+                cellValue = person["first_name"] + " " + person["last_name"];
+                break;                
+              case 2:
+                console.log("two");
+                cellValue = order["drink"]["name"];          
+                break;
+            }
+            
+            let text = document.createTextNode(cellValue);
             cell.appendChild(text);
         }
     }
@@ -27,6 +45,7 @@ function generateTableBody(table, data) {
 function viewRoundOrders(event, round_id) {
     event.preventDefault();
     modal = document.getElementById("orders_modal");
+    // modal_round_id_span = document.getElementById("modal_round_id_span");
     close_btn = document.getElementById("close_modal").addEventListener("click", function() {
         modal.style.display = "none";
     });
